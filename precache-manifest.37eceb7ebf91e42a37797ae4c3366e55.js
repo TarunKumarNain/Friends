06c807d3e698e30f0067f56ebb3d4412b2884064
Tarun Kumar <tarunnain467@gmail.com>
2020-04-21 23:34:12 -0800

    Updates

---
+++ precache-manifest.37eceb7ebf91e42a37797ae4c3366e55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a39187ef3b2994bbed70be7d24282450",
+    "url": "/Friends/index.html"
+  },
+  {
+    "revision": "4365d0075be0241d6d3a",
+    "url": "/Friends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "2b3cbb96366477e5a797",
+    "url": "/Friends/static/css/main.88a2ba76.chunk.css"
+  },
+  {
+    "revision": "4365d0075be0241d6d3a",
+    "url": "/Friends/static/js/2.84d6e4e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Friends/static/js/2.84d6e4e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b3cbb96366477e5a797",
+    "url": "/Friends/static/js/main.68c67a27.chunk.js"
+  },
+  {
+    "revision": "2181317d22d854ff7e61",
+    "url": "/Friends/static/js/runtime-main.0805b6ab.js"
+  }
+]);
